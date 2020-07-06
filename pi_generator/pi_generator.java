2ce2491290b7840a78797d912a29f6fecbf3a7f4
C411XM3XN00B <30630624+C411XM3XN00B@users.noreply.github.com>
2020-07-06 20:03:41 +0200

    Update pi_generator.java
    Improved precision by using variable type double and increasing iteration count

--- pi_generator/pi_generator.java
+++ pi_generator/pi_generator.java
@@ -5,22 +5,19 @@ public class pi_generator {
 
     public static void main(String[] args) {
 
-        double pi = 3.0f;
+        double pi = 3.0;
         long n = 2;
         int x = 1;
 
-        System.out.println("test");
+        int iterations = 132000;
 
-        for (long i = 0; i < 1000; i++) {
-            pi = pi + x *(4.0f/(n*(n+1.0f)*(n+2.0f)));
+        System.out.print("Approximation of Pi: ");
+
+        for (long i = 0; i < iterations; i++) {
+            pi = pi + x *(4.0/(n*(n+1.0)*(n+2.0)));
             n = n+2;
             x = -x;
           }
           System.out.println(pi);
-
-
-
-
-
     }
 }
